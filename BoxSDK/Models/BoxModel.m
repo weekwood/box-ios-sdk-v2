aff66c0e770bf9a467ea5d0671cc2b24238d06bf
Ryan Lopopolo <rjl@hyperbo.la>
2013-08-18 04:41:32 -0700

    Fix a crash when parsing dates
    Don't attempt to parse a date timestamp if it is nil.

--- BoxSDK/Models/BoxModel.m
+++ BoxSDK/Models/BoxModel.m
@@ -44,7 +44,10 @@
     }
 
     NSDate *returnDate = nil;
-    returnDate = [dateFormatter dateFromString:timestamp];
+    if (timestamp != nil)
+    {
+        returnDate = [dateFormatter dateFromString:timestamp];
+    }
 
     return returnDate;
 }
